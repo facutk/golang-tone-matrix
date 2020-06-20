d34391c3a7bf65ce73dfa8c800fd9aca9425fca1
jtkaczyszyn <jtkaczyszyn@elementum.com>
2020-06-20 01:13:25 -0300

    ping multiple times on dev

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
+	timestamp := time.Now().Unix()
+	isDev := os.Getenv("APP_ENV") == "dev"
 	port := os.Getenv("PORT")
 
 	if port == "" {
@@ -23,12 +26,19 @@ func main() {
 	router.StaticFile("/favicon.ico", "./static/favicon.ico")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		c.HTML(
+			http.StatusOK,
+			"index.tmpl.html",
+			gin.H{
+				"isDev":            isDev,
+				"initialTimestamp": timestamp,
+			})
 	})
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message":   "pong",
+			"timestamp": timestamp,
 		})
 	})
 

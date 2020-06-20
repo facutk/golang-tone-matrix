006cb14a26a023cf4b73e0c0891c375b4fed6f63
jtkaczyszyn <jtkaczyszyn@elementum.com>
2020-06-20 01:17:22 -0300

    remove ping code

--- main.go
+++ main.go
@@ -4,15 +4,12 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
-	timestamp := time.Now().Unix()
-	isDev := os.Getenv("APP_ENV") == "dev"
 	port := os.Getenv("PORT")
 
 	if port == "" {
@@ -26,19 +23,12 @@ func main() {
 	router.StaticFile("/favicon.ico", "./static/favicon.ico")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(
-			http.StatusOK,
-			"index.tmpl.html",
-			gin.H{
-				"isDev":            isDev,
-				"initialTimestamp": timestamp,
-			})
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message":   "pong",
-			"timestamp": timestamp,
+			"message": "pong",
 		})
 	})
 
